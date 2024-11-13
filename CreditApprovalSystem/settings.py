727b6c5434955ff40fec903302a705ae0cb86b2b
Raju1422 <raju7981435916@gmail.com>
2024-11-13 11:44:44 +0530

    PostgreSQL Database integrated

--- CreditApprovalSystem/settings.py
+++ CreditApprovalSystem/settings.py
@@ -76,9 +76,13 @@ WSGI_APPLICATION = 'CreditApprovalSystem.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+  'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Alemeno', 
+        'USER': 'Santosh',
+        'PASSWORD': 'sanju@169',
+        'HOST': 'localhost', 
+        'PORT': '5432',
     }
 }
 
